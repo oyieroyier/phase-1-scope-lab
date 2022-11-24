d3ec2107f9374d120d9be663d52869bbc02cc34e
Bob Oyier <boboyier92@gmail.com>
2022-11-24 23:19:05 +0300

    "completed Moringa School's JavaScript scope lab."

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "She Hulk"
+
+function upperCaseCustomerName() {
+  customerName = "BOB";
+}
+
+function setBestCustomer() {
+  return bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+	return bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer() {
+	return leastFavoriteCustomer = "Captain Marvel"
+}
